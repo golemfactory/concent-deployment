b3a3fe121c7a9d588291f207cdd0602a9304d2ac
cameel <cameel2@gmail.com>
2017-11-07 17:40:18 +0100

    kubernetes: mail-relay service and deployment

--- kubernetes/delete-services.sh
+++ kubernetes/delete-services.sh
@@ -2,5 +2,6 @@
 
 kubectl delete --filename services/nginx-proxy.yml || true
 kubectl delete --filename services/concent-api.yml || true
+kubectl delete --filename services/mail-relay.yml  || true
 
 ./delete-config-maps.sh
