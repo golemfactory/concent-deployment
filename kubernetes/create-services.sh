c5f7f4647599516d690904e680303f089f1ed3a6
bartoszbetka <bartoszbetka94@gmail.com>
2017-12-01 11:30:04 +0100

    nginx-storage pod with persistent disk

--- kubernetes/create-services.sh
+++ kubernetes/create-services.sh
@@ -2,5 +2,6 @@
 
 ./create-config-maps.sh
 
+kubectl create --record --filename services/nginx-storage.yml
 kubectl create --record --filename services/concent-api.yml
 kubectl create --record --filename services/nginx-proxy.yml
