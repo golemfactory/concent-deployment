da7654a6e8f147e4d7105c014669fe2e26ca670d
bartoszbetka <bartoszbetka94@gmail.com>
2018-01-10 18:19:51 +0100

    Add kubernetes service and deployment for geth and edit Makefile to build it

--- kubernetes/delete-services.sh
+++ kubernetes/delete-services.sh
@@ -5,5 +5,6 @@ kubectl delete --filename services/nginx-proxy.yml   || true
 kubectl delete --filename services/nginx-storage.yml || true
 kubectl delete --filename services/gatekeeper.yml    || true
 kubectl delete --filename services/concent-api.yml   || true
+kubectl delete --filename services/geth.yml          || true
 
 ./delete-config-maps.sh || true
