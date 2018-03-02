5e9d451e1e07dd2ebc78d7f1231be391176048bf
bartoszbetka <bartoszbetka94@gmail.com>
2018-03-02 10:51:16 +0100

    Create and add file with concent version to container that run the Django application

--- containers/package-builder/build-concent-api.sh
+++ containers/package-builder/build-concent-api.sh
@@ -3,9 +3,13 @@
 concent_dir=$1
 output_dir=$2
 
+build/virtualenv/bin/python3 "build/virtualenv/src/golem-messages/version.py"
+mv RELEASE-VERSION "$concent_dir"
+
 tar                                            \
     --create                                   \
     --verbose                                  \
     --file="$output_dir/concent-api.tar"       \
     --directory="$concent_dir"                 \
-    concent_api/
+    concent_api/                               \
+    RELEASE-VERSION
---
+++ containers/package-builder/build-virtualenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+requirements_path=$(realpath "$1")
+build_dir=$(realpath "$2")
+
+mkdir --parents "$build_dir/virtualenv/"
+
+virtualenv --python python3 "$build_dir/virtualenv/"
+source "$build_dir/virtualenv/bin/activate"
+pip install -r "$requirements_path"
