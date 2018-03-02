ffde132446c609d397c68b1a855d5917b9137d40
bartoszbetka <bartoszbetka94@gmail.com>
2018-03-02 15:52:08 +0100

    Add json errors and accept header for nginx-proxy and nginx-storage

--- kubernetes/delete-config-maps.sh
+++ kubernetes/delete-config-maps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
-kubectl delete configmap nginx-storage-config || true
-kubectl delete configmap nginx-config         || true
-kubectl delete configmap gatekeeper-settings  || true
-kubectl delete configmap concent-api-settings || true
-kubectl delete secret concent-api-secrets     || true
+kubectl delete configmap nginx-config-snippets || true
+kubectl delete configmap nginx-storage-config  || true
+kubectl delete configmap nginx-config          || true
+kubectl delete configmap gatekeeper-settings   || true
+kubectl delete configmap concent-api-settings  || true
+kubectl delete secret concent-api-secrets      || true
