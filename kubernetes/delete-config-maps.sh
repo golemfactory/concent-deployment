912a1c4cc280d3ebe6a333b50b4041132dc0bcc7
bartoszbetka <bartoszbetka94@gmail.com>
2018-03-09 11:23:14 +0100

    Add ssl support for nginx-proxy

--- kubernetes/delete-config-maps.sh
+++ kubernetes/delete-config-maps.sh
@@ -2,7 +2,8 @@
 
 kubectl delete configmap nginx-config-snippets || true
 kubectl delete configmap nginx-storage-config  || true
-kubectl delete configmap nginx-config          || true
+kubectl delete secret nginx-proxy-secrets      || true
+kubectl delete configmap nginx-configs         || true
 kubectl delete configmap gatekeeper-settings   || true
 kubectl delete configmap concent-api-settings  || true
 kubectl delete secret concent-api-secrets      || true
