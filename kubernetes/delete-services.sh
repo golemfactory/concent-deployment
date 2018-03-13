cd574bfb845d82777e1c5f1eea2a35f25dd7e8a0
bartoszbetka <bartoszbetka94@gmail.com>
2018-03-13 18:07:04 +0100

    Add deployment and service with rabbitmq on the cluster

--- kubernetes/delete-services.sh
+++ kubernetes/delete-services.sh
@@ -5,6 +5,7 @@ kubectl delete --filename services/nginx-proxy.yml   || true
 kubectl delete --filename services/nginx-storage.yml || true
 kubectl delete --filename services/gatekeeper.yml    || true
 kubectl delete --filename services/concent-api.yml   || true
+kubectl delete --filename services/rabbitmq.yml      || true
 kubectl delete --filename services/geth.yml          || true
 
 ./delete-config-maps.sh || true
