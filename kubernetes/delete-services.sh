3dc2b5dc830f22922ad6a8c796ee6a67e61dd8bd
bartoszbetka <bartoszbetka94@gmail.com>
2018-03-26 19:57:34 +0200

    Add kubernetes service and deployment for verification images

--- kubernetes/delete-services.sh
+++ kubernetes/delete-services.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 
+kubectl delete --filename services/verifier.yml      || true
 kubectl delete --filename services/nginx-proxy.yml   || true
 kubectl delete --filename services/nginx-storage.yml || true
 kubectl delete --filename services/gatekeeper.yml    || true
