379d2bf0e333350b02930b972a68da4c62e97aed
bartoszbetka <bartoszbetka94@gmail.com>
2018-04-05 18:39:58 +0200

    Add the possibility of choosing a database

--- kubernetes/create-job-dependencies.sh
+++ kubernetes/create-job-dependencies.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
-kubectl create --filename secrets/db-secrets.yml
+cluster_type=$1
+
+kubectl create configmap database-job-settings                                \
+    --from-file=local_settings.py=config-maps/database-jobs/local_settings.py \
+    --from-literal=__init__.py=
+kubectl create --filename "secrets/$cluster_type-db-secrets.yml"
+if [[ $cluster_type == "control" ]]; then
 kubectl create secret generic django-admin-fixture \
     --from-file=django-admin-fixture.yaml=secrets/django-admin-fixture.yaml
+fi
