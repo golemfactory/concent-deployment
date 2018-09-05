946e753da2c87e6a8ac8df7f1a295ff081889aeb
bartoszbetka <bartoszbetka94@gmail.com>
2018-09-05 17:15:38 +0200

    Add a custom yash filter to decode the secrets encoded in base64

---
+++ tools/jinja2-filters.py
@@ -0,0 +1,20 @@
+from base64 import b64decode
+
+class NotAString(Exception):
+    pass
+
+class UnsupportedDataType(Exception):
+    pass
+
+def filter_b64decode(encoded_value, return_type = "string"):
+    if not isinstance(encoded_value, str):
+        raise NotAString("The encoded value does not a string")
+    if return_type == "string":
+        # If the input is invalid, decoding can result in the following exceptions:
+        # - binascii.Error     - if the input is not base64 encoded
+        # - UnicodeDecodeError - if the encoded value is not a valid UTF-8 string
+        return b64decode(encoded_value, validate = True).decode("utf-8")
+    if return_type == "bytes":
+        return b64decode(encoded_value, validate = True)
+    else:
+        raise UnsupportedDataType("The encoded value is an unsupported data type")
