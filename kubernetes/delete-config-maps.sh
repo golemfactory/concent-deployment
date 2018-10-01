1da80a038acf50a676d94334597e8577f67ce411
bartoszbetka <bartoszbetka94@gmail.com>
2018-10-01 12:24:40 +0200

    Delete old, not existing configmap from the `delete-config-maps` file

--- kubernetes/delete-config-maps.sh
+++ kubernetes/delete-config-maps.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-kubectl delete configmap nginx-config-snippets        || true
 kubectl delete configmap nginx-storage-configs        || true
 kubectl delete configmap file-transfer-config         || true
 kubectl delete configmap nginx-configs                || true
