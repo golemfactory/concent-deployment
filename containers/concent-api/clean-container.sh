3cae834845ed016025403f2f9e38d737519e4ab5
bartoszbetka <bartoszbetka94@gmail.com>
2019-01-30 12:44:31 +0100

    Fix the order of the clean up commands in docker images
    The `apt autoremove` command should be the last command, because it
    clean up dependencies that are not uses after uninstall specific
    packages from system.

--- containers/concent-api/clean-container.sh
+++ containers/concent-api/clean-container.sh
@@ -5,7 +5,7 @@ readarray temporary_dependencies < ${BASH_SOURCE%/*}/build-dependencies.txt
 
 # Clean up
 apt-get clean
-apt autoremove --assume-yes
 apt-get remove --purge --assume-yes ${temporary_dependencies[*]}
+apt autoremove --assume-yes
 
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
