ca97efa6ddde28d6d9b0d9124cc5f852494ff0dd
bartoszbetka <bartoszbetka94@gmail.com>
2019-01-30 12:31:22 +0100

    Move the `clean-container.sh` script to concent-api image, because its sources are used for another images

--- containers/concent-api/clean-container.sh
+++ containers/concent-api/clean-container.sh
@@ -9,7 +9,3 @@ apt autoremove --assume-yes
 apt-get remove --purge --assume-yes ${temporary_dependencies[*]}
 
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Temporary file which is needed to build middleman protocol
-rm /usr/lib/signing_service/RELEASE-VERSION
-
