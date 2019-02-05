2139aee6c33bb7f86461a693b1cb7d30167134b0
Kamil Śliwak <kamil.sliwak@codepoets.it>
2019-02-05 13:49:37 +0100

    fixup! Add desktop environment to virtual machine for possibility of running golem gui interface

--- concent-vm/helper-scripts/golem-run-gui-mode.sh
+++ concent-vm/helper-scripts/golem-run-gui-mode.sh
@@ -1,13 +1,12 @@
-#! /bin/bash -e
+#!/bin/bash -e
 
 trap "kill 0" SIGINT
 
-golem_run_gui_command="$(
-    golem-run-console-mode.sh $@ &    \
-    cd ~/golem_electron/;             \
-    npm run start:app &               \
-    npm run start:mainnet &           \
-)"
 
-
-$golem_run_gui_command
+# Running the commands in $() allows our trap to kill them all on exit
+result=$(
+    golem-run-console-mode.sh $@ & \
+    cd ~/golem_electron/;          \
+    npm run start:app &            \
+    npm run start:mainnet &        \
+)
