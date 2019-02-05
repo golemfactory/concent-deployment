b2d2f36a1d7a6d60ef196c86cc03d4ce26a3153a
Kamil Śliwak <kamil.sliwak@codepoets.it>
2019-02-05 13:50:56 +0100

    golem-run-gui-mode: Set DISPLAY so that the script can be started from the text console

--- concent-vm/helper-scripts/golem-run-gui-mode.sh
+++ concent-vm/helper-scripts/golem-run-gui-mode.sh
@@ -2,6 +2,9 @@
 
 trap "kill 0" SIGINT
 
+# This is the display number of the X server that starts by default in our virtual machine.
+# It's not universal and may not work if this script in executed in a different environment.
+export DISPLAY=:0.0
 
 # Running the commands in $() allows our trap to kill them all on exit
 result=$(
