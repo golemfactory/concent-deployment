32ef11d7cb5494bd10dff24f4c3b37b08c93b770
bartoszbetka <bartoszbetka94@gmail.com>
2020-01-30 12:22:52 +0100

    Increase timeout for terminating kubernetes pods

--- kubernetes/delete-services.sh
+++ kubernetes/delete-services.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -e
 
 
-kubectl delete --wait=true --timeout=30s --ignore-not-found=true --filename services/verifier.yml
-kubectl delete --wait=true --timeout=30s --ignore-not-found=true --filename services/nginx-proxy.yml
-kubectl delete --wait=true --timeout=30s --ignore-not-found=true --filename services/nginx-storage.yml
-kubectl delete --wait=true --timeout=30s --ignore-not-found=true --filename services/gatekeeper.yml
-kubectl delete --wait=true --timeout=30s --ignore-not-found=true --filename services/conductor.yml
-kubectl delete --wait=true --timeout=30s --ignore-not-found=true --filename services/conductor-worker.yml
-kubectl delete --wait=true --timeout=30s --ignore-not-found=true --filename services/concent-api-worker.yml
-kubectl delete --wait=true --timeout=30s --ignore-not-found=true --filename services/concent-api.yml
-kubectl delete --wait=true --timeout=30s --ignore-not-found=true --filename services/middleman.yml
-kubectl delete --wait=true --timeout=30s --ignore-not-found=true --filename services/signing-service.yml
-kubectl delete --wait=true --timeout=30s --ignore-not-found=true --filename services/rabbitmq.yml
-kubectl delete --wait=true --timeout=30s --ignore-not-found=true --filename services/geth.yml
+kubectl delete --wait=true --timeout=60s --ignore-not-found=true --filename services/verifier.yml
+kubectl delete --wait=true --timeout=60s --ignore-not-found=true --filename services/nginx-proxy.yml
+kubectl delete --wait=true --timeout=60s --ignore-not-found=true --filename services/nginx-storage.yml
+kubectl delete --wait=true --timeout=60s --ignore-not-found=true --filename services/gatekeeper.yml
+kubectl delete --wait=true --timeout=60s --ignore-not-found=true --filename services/conductor.yml
+kubectl delete --wait=true --timeout=60s --ignore-not-found=true --filename services/conductor-worker.yml
+kubectl delete --wait=true --timeout=60s --ignore-not-found=true --filename services/concent-api-worker.yml
+kubectl delete --wait=true --timeout=60s --ignore-not-found=true --filename services/concent-api.yml
+kubectl delete --wait=true --timeout=60s --ignore-not-found=true --filename services/middleman.yml
+kubectl delete --wait=true --timeout=60s --ignore-not-found=true --filename services/signing-service.yml
+kubectl delete --wait=true --timeout=60s --ignore-not-found=true --filename services/rabbitmq.yml
+kubectl delete --wait=true --timeout=60s --ignore-not-found=true --filename services/geth.yml
 kubectl wait --for=delete pods --all || true
 
 ./delete-config-maps.sh || true
